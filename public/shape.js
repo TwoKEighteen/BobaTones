c3c13fb54dd276b0972ad325f27038e449672c54
Iggyfufu <kennethzhu07@gmail.com>
2018-06-15 10:47:48 -0400

    fix(linter errors)

--- public/shape.js
+++ public/shape.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function() {
   const canvas = this.document.getElementById('myCanvas')
   paper.setup(canvas)
 
@@ -15,51 +15,67 @@ window.onload = function () {
       fillColor: {
         hue: Math.random() * 360,
         saturation: 1,
-        brightness: 0,
+        brightness: 0
       },
-      blendMode: 'lighter',
+      blendMode: 'lighter'
     })
 
     for (let i = 0; i < this.numSegment; i++) {
       this.boundOffset.push(this.radius)
       this.boundOffsetBuff.push(this.radius)
       this.path.add(new paper.Point())
-      this.sidePoints.push(new paper.Point({
-        angle: 360 / this.numSegment * i,
-        length: 1,
-      }))
+      this.sidePoints.push(
+        new paper.Point({
+          angle: 360 / this.numSegment * i,
+          length: 1
+        })
+      )
     }
   }
 
   Ball.prototype = {
-
     iterate() {
       this.checkBorders()
-      if (this.vector.length > this.maxVec) { this.vector.length = this.maxVec }
+      if (this.vector.length > this.maxVec) {
+        this.vector.length = this.maxVec
+      }
       this.point = this.point.add(this.vector)
       this.updateShape()
     },
 
     checkBorders() {
       const size = paper.view.size
-      if (this.point.x < -this.radius) { this.point.x = size.width + this.radius }
-      if (this.point.x > size.width + this.radius) { this.point.x = -this.radius }
-      if (this.point.y < -this.radius) { this.point.y = size.height + this.radius }
-      if (this.point.y > size.height + this.radius) { this.point.y = -this.radius }
+      if (this.point.x < -this.radius) {
+        this.point.x = size.width + this.radius
+      }
+      if (this.point.x > size.width + this.radius) {
+        this.point.x = -this.radius
+      }
+      if (this.point.y < -this.radius) {
+        this.point.y = size.height + this.radius
+      }
+      if (this.point.y > size.height + this.radius) {
+        this.point.y = -this.radius
+      }
     },
 
     updateShape() {
       const segments = this.path.segments
-      for (var i = 0; i < this.numSegment; i++) { segments[i].point = this.getSidePoint(i) }
+      for (var i = 0; i < this.numSegment; i++) {
+        segments[i].point = this.getSidePoint(i)
+      }
 
       this.path.smooth()
       for (var i = 0; i < this.numSegment; i++) {
-        if (this.boundOffset[i] < this.radius / 4) { this.boundOffset[i] = this.radius / 4 }
+        if (this.boundOffset[i] < this.radius / 4) {
+          this.boundOffset[i] = this.radius / 4
+        }
         const next = (i + 1) % this.numSegment
-        const prev = (i > 0) ? i - 1 : this.numSegment - 1
+        const prev = i > 0 ? i - 1 : this.numSegment - 1
         let offset = this.boundOffset[i]
         offset += (this.radius - offset) / 15
-        offset += ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3
+        offset +=
+          ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3
         this.boundOffsetBuff[i] = this.boundOffset[i] = offset
       }
     },
@@ -68,7 +84,7 @@ window.onload = function () {
       const dist = this.point.getDistance(b.point)
       if (dist < this.radius + b.radius && dist != 0) {
         const overlap = this.radius + b.radius - dist
-        const direc = (this.point.subtract(b.point)).normalize(overlap * 0.015)
+        const direc = this.point.subtract(b.point).normalize(overlap * 0.015)
         this.vector = this.vector.add(direc)
         b.vector = b.vector.subtract(direc)
 
@@ -102,10 +118,11 @@ window.onload = function () {
     },
 
     updateBounds() {
-      for (let i = 0; i < this.numSegment; i++) { this.boundOffset[i] = this.boundOffsetBuff[i] }
-    },
+      for (let i = 0; i < this.numSegment; i++) {
+        this.boundOffset[i] = this.boundOffsetBuff[i]
+      }
+    }
   }
-
 
   const balls = []
   const numBalls = 10
@@ -113,13 +130,13 @@ window.onload = function () {
     const position = paper.Point.random().multiply(paper.view.size)
     const vector = new paper.Point({
       angle: Math.floor(360 * Math.random()),
-      length: Math.floor(Math.random() * 10),
+      length: Math.floor(Math.random() * 10)
     })
     const radius = Math.random() * 60 + 60
     balls.push(new Ball(radius, position, vector))
   }
 
-  paper.view.onFrame = function () {
+  paper.view.onFrame = function() {
     for (var i = 0; i < balls.length - 1; i++) {
       for (let j = i + 1; j < balls.length; j++) {
         balls[i].react(balls[j])
@@ -130,4 +147,3 @@ window.onload = function () {
     }
   }
 }
-
