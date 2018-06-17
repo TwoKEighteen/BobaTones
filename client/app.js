ac1768fcc543aea1aefcd04bffd6c61e3fa12370
Iggyfufu <kennethzhu07@gmail.com>
2018-06-17 15:15:21 -0400

    added home page styling

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Header, Button, Image, Grid, Icon} from 'semantic-ui-react'
 import BobaStuff from './Boba'
-import {Component} from 'react'
 
 class App extends Component {
   constructor() {
@@ -18,12 +18,24 @@ class App extends Component {
 
   render() {
     return !this.state.start ? (
-      <div>
-        <h3>Boba Tone!</h3>
-        <button type="submit" onClick={this.handleClick}>
-          start
-        </button>
-      </div>
+      <Grid centered columns={3}>
+        <Grid.Column>
+          <Image
+            size="massive"
+            src="https://piskel-imgstore-b.appspot.com/img/a52f91dc-725b-11e8-b529-1964359b2cf9.gif"
+          />
+          <Header as="h1" textAlign="center">
+            {' '}
+            Boba Tone{' '}
+          </Header>
+          <Button animated fluid onClick={this.handleClick}>
+            <Button.Content visible>Play</Button.Content>
+            <Button.Content hidden>
+              <Icon name="play" />
+            </Button.Content>
+          </Button>
+        </Grid.Column>
+      </Grid>
     ) : (
       <BobaStuff />
     )
